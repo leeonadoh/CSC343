0918c6a2d5198b3e5b6dd5dad62295a2644fcc60
Leo Li <leo.sunpeng.li@gmail.com>
2014-03-07 15:58:59 -0500

    updated java

--- src/assignment2/embededSql/Assignment2.java
+++ src/assignment2/embededSql/Assignment2.java
@@ -1,6 +1,11 @@
 package assignment2.embededSql; //TODO delete before handing in.
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Assignment2 {
 
@@ -56,13 +61,15 @@ public class Assignment2 {
 		}
 	}
 
-	// Inserts row into the winemaker table
+	// Inserts row into the country table
 	public boolean insertCountry(int cid, String name, String coach) {
 		try {
-			String sqlText = ("INSERT INTO country " + "VALUES (" + cid + ", '"
-					+ name + "', '" + coach + "')");
+			String sqlText = ("INSERT INTO country "
+					+ "VALUES (" + cid + ", '" + name + "', '" + coach + "')");
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			sql.executeUpdate(sqlText);
+			releaseResources();
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -75,12 +82,13 @@ public class Assignment2 {
 			int result;
 			String sqlText = ("SELECT SUM(pid) AS cnt FROM player GROUP BY cid");
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			rs = sql.executeQuery(sqlText);
 			if (rs.first())
 				result = rs.getInt("cnt");
 			else
 				result = -1;
-			rs.close();
+			releaseResources();
 			return result;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -94,6 +102,7 @@ public class Assignment2 {
 			String sqlText = ("SELECT fname, lname, position, goals "
 					+ "FROM player WHERE pid = " + pid);
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			rs = sql.executeQuery(sqlText);
 			if (rs.first())
 				result = (rs.getString("fname") + ":" + rs.getString("lname")
@@ -101,7 +110,7 @@ public class Assignment2 {
 						.getString("goals"));
 			else
 				result = "";
-			rs.close();
+			releaseResources();
 			return result;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -114,8 +123,10 @@ public class Assignment2 {
 			String sqlText = ("UPDATE stadium SET city = '" + newCity
 					+ "' WHERE sid = " + sid);
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			int updated = sql.executeUpdate(sqlText);
 			System.out.println(updated + " rows were updated.");
+			releaseResources();
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -127,8 +138,10 @@ public class Assignment2 {
 		try {
 			String sqlText = ("DELETE FROM country WHERE cid = " + cid);
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			int updated = sql.executeUpdate(sqlText);
 			System.out.println(updated + " rows were updated.");
+			releaseResources();
 			return true;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -143,6 +156,7 @@ public class Assignment2 {
 					+ "WHERE p.cid = c.cid AND p.fcid = b.fcid AND "
 					+ "b.name = '" + fcname + "'");
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			rs = sql.executeQuery(sqlText);
 			String result = "";
 			while (rs.next()) {
@@ -151,8 +165,10 @@ public class Assignment2 {
 						+ rs.getString("po") + ":" + rs.getString("go") + ":"
 						+ rs.getString("cn") + "#");
 			}
-			rs.close();
-			return result;
+			releaseResources();
+			// Strip last pound sign.
+			return result.isEmpty() ? result : result.substring(0,
+					result.length() - 1);
 		} catch (SQLException e) {
 			e.printStackTrace();
 			return "";
@@ -162,21 +178,21 @@ public class Assignment2 {
 	public boolean updateValues(String cname, int incrV) {
 		try {
 			boolean updated = false;
-			String sqlText = 
-					("SELECT p.pid AS pid, p.value AS val "
+			String sqlText = ("SELECT p.pid AS pid, p.value AS val "
 					+ "FROM player AS p, country AS c "
 					+ "WHERE p.cid = c.cid AND c.name = '" + cname + "'");
 
 			System.out.println("Executing command: " + sqlText);
+			connectStatement();
 			rs = sql.executeQuery(sqlText);
-			while (rs.next()){
+			while (rs.next()) {
 				int pid = rs.getInt("pid");
 				int val = rs.getInt("val") + incrV;
 				sql.executeUpdate("UPDATE player SET value = " + val
 						+ " WHERE pid = " + pid);
 				updated = true;
 			}
-			rs.close();
+			releaseResources();
 			return updated;
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -185,11 +201,92 @@ public class Assignment2 {
 	}
 
 	public String query7() {
-		return "";
+		try {
+			String view1 = "CREATE VIEW LowestBudgetTeam AS "
+					+ "Select cid, budget "
+					+ "FROM CountryBudget "
+					+ "WHERE budget = (SELECT min(budget) from CountryBudget)";
+
+			String view1Drop = "DROP VIEW LowestBudgetTeam";
+
+			String view2 = "CREATE VIEW TopScorer AS "
+					+ "SELECT pid, cid "
+					+ "FROM player "
+					+ "WHERE goals = (SELECT max(goals) FROM player)";
+
+			String view2Drop = "DROP VIEW TopScorer";
+
+			String query = "SELECT c.name as name, c.coach as coach, cb.budget as budget "
+					+ "FROM Country as c, LowestBudgetTeam as cb, TopScorer as ts "
+					+ "WHERE c.cid = cb.cid AND cb.cid = ts.cid";
+
+			connectStatement();
+			System.out.println("Creating View: " + view1);
+			sql.executeUpdate(view1);
+			System.out.println("Creating View: " + view2);
+			sql.executeUpdate(view2);
+			System.out.println("Executing Query: " + query);
+			rs = sql.executeQuery(query);
+			String result = "";
+			while (rs.next()) {
+				result += (rs.getString("name") + ":" + rs.getString("coach")
+						+ ":" + rs.getString("budget") + "#");
+			}
+			sql.executeUpdate(view1Drop);
+			sql.executeUpdate(view2Drop);
+			releaseResources();
+			// Strip last pound sign.
+			return result.isEmpty() ? result : result.substring(0,
+					result.length() - 1);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 	public boolean updateDB() {
-		return false;
+		try {
+			String query = "SELECT DISTINCT p.pid AS pid, p.lname AS ln"
+					+ "FROM player AS p, appearance AS a "
+					+ "WHERE p.pid = a.pid AND a.minutes = 90 "
+					+ "ORDER BY pid";
+			System.out.println("Executing Query: " + query);
+			connectStatement();
+			rs = sql.executeQuery(query);
+
+			System.out.println("Creating table valueablePlayers");
+			String create = "CREATE TABLE valuablePlayers (pid INTEGER, lname VARCHAR(20))";
+			sql.executeUpdate(create);
+
+			String insert = "INSERT INTO valuablePlayers VALUES (?,?)";
+			System.out.println("Prepared Statement: " + insert);
+			ps = connection.prepareStatement(insert);
+			while(rs.next()){
+				// Populate prepared statement.
+				ps.setInt(1, rs.getInt("pid"));
+				ps.setString(2, rs.getString("ln"));
+				// Execute insert.
+				ps.executeUpdate();
+			}
+			releaseResources();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	private void releaseResources() throws SQLException {
+		if (!sql.isClosed())
+			sql.close();
+		if (!rs.isClosed())
+			rs.close();
+		if (!ps.isClosed())
+			ps.close();
+	}
+
+	private void connectStatement() throws SQLException {
+		sql = connection.createStatement();
 	}
 
 }
